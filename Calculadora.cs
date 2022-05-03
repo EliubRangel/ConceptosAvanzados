2e93d9ff6476bbd897982a2f3a8aadb44cd7571a
Eliub <eliubr47@gmail.com>
2022-05-02 20:59:25 -0500

    se creo programa para ver excepciones

---
+++ Calculadora.cs
@@ -0,0 +1,13 @@
+namespace ConceptosAvanzados
+{
+    public class Calculadora
+    {
+        public decimal Dividir(decimal num1, decimal num2)
+        {
+            decimal division= num1/num2;
+            return division;
+        }
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConceptosAvanzados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora");
+            Calculadora cal = new Calculadora();
+            try
+            {
+                decimal x = decimal.Parse(Console.ReadLine());
+                decimal division = cal.Dividir(5,x);
+                Console.WriteLine(division);
+            }
+            catch(DivideByZeroException de)
+            {
+                Console.WriteLine("Error al dividir por cero");
+            }
+            catch(FormatException fe)
+            {
+                Console.WriteLine("Error de formato no numerico");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Hubo un error inesperado");
+            }
+            finally
+            {
+                Console.WriteLine("Terminado");
+            }
+
+            Console.WriteLine("Adios");
+        }
+    }
+}
